8fe0ea47311f0a343e4994e3ac1a297e4abfb0d8
Odair José Gambert Nossa <odairjgn@outlook.com>
2024-03-03 16:50:52 -0300

    Add prime flex

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,11 @@
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+root.render(<App />);
+
+function App() {
+  return <h2>Hello from React!</h2>;
+}
