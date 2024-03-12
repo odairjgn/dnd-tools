8eaa3afa5a9b797046230c44b8c589bddfe1429a
Odair José Gambert Nossa <odairjgn@outlook.com>
2024-03-12 00:11:21 -0300

    Ajustes

---
+++ src/components/dices/d20/index.tsx
@@ -0,0 +1,32 @@
+import "./styles.scss";
+
+export function D20() {
+  return (
+    <div>
+      <div className="content">
+        <div className="die">
+          <figure className="face face-1"></figure>
+          <figure className="face face-2"></figure>
+          <figure className="face face-3"></figure>
+          <figure className="face face-4"></figure>
+          <figure className="face face-5"></figure>
+          <figure className="face face-6"></figure>
+          <figure className="face face-7"></figure>
+          <figure className="face face-8"></figure>
+          <figure className="face face-9"></figure>
+          <figure className="face face-10"></figure>
+          <figure className="face face-11"></figure>
+          <figure className="face face-12"></figure>
+          <figure className="face face-13"></figure>
+          <figure className="face face-14"></figure>
+          <figure className="face face-15"></figure>
+          <figure className="face face-16"></figure>
+          <figure className="face face-17"></figure>
+          <figure className="face face-18"></figure>
+          <figure className="face face-19"></figure>
+          <figure className="face face-20"></figure>
+        </div>
+      </div>
+    </div>
+  );
+}
